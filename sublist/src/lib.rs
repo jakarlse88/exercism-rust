8bcb95ee35eac207b93c5b0b5cb02fcd32abb2e1
Jon Karlsen <karlsen.jon@icloud.com>
2023-07-26 22:38:49 +0900

    SUBLIST: refactor

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,30 +7,12 @@ pub enum Comparison {
     }
 
 
-enum LengthComparison {
-    Equal   = 0
-  , ALonger = -1
-  , BLonger = 1
+fn should_eval_sublist<T>( arr_a: &[ T ] , arr_b : &[ T ] ) -> bool {
+    ( arr_a.len() as i8 - arr_b.len() as i8 ) < 0
 }
 
 
-fn compare_length<T>( arr_a: &[ T ] , arr_b : &[ T ] ) -> LengthComparison {
-    let diff = arr_a.len() as i32 - arr_b.len() as i32;
-
-    return 
-        if diff == 0 {
-            LengthComparison::Equal
-        }
-        else if diff > 0 {
-            LengthComparison::ALonger
-        }
-        else {
-            LengthComparison::BLonger
-        }
-}
-
-
-fn compare<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> bool {
+fn occurs_in<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> bool {
     let mut i : usize = 0;
     let mut k : usize = arr_a.len();
 
@@ -38,8 +20,6 @@ fn compare<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> bool {
         if k > arr_b.len() {
             break;
         }
-
-        let _temp = &arr_b[ i .. k ];
 
         if arr_a == &arr_b[ i .. k ] {
             return true
@@ -53,32 +33,26 @@ fn compare<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> bool {
 }
 
 
-fn compare_equal<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> Comparison {
-    return 
-        if arr_a == arr_b {
-            Comparison::Equal
-        }
-        else {
-            Comparison::Unequal
-        }
+fn eval_sublist<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> Comparison {
+    let is_sublist = arr_a.is_empty() 
+                  || occurs_in( arr_a , arr_b );
+
+    if   is_sublist { Comparison::Sublist } 
+    else            { Comparison::Unequal }
 }
 
+
+fn eval_superlist<T : Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> Comparison {
+    let is_superlist = arr_b.is_empty() 
+                    || occurs_in( arr_b , arr_a );
+
+    if   is_superlist { Comparison::Superlist }   
+    else              { Comparison::Unequal   }
+}
+
+
 pub fn sublist<T : PartialEq + Eq>( arr_a: &[ T ] , arr_b : &[ T ] ) -> Comparison {
-    if arr_a == arr_b {
-        return Comparison::Equal
-    }
-
-    if arr_a.is_empty() {
-        return Comparison::Sublist
-    }
-
-    if arr_b.is_empty() {
-        return Comparison::Superlist
-    }
-
-    return match compare_length( arr_a , arr_b ) {
-        LengthComparison::Equal   => compare_equal( arr_a , arr_b )
-      , LengthComparison::ALonger => if compare( arr_b , arr_a ) { Comparison::Superlist } else { Comparison::Unequal }
-      , LengthComparison::BLonger => if compare( arr_a , arr_b ) { Comparison::Sublist }   else { Comparison::Unequal }
-    }
+    if      arr_a == arr_b                       { Comparison::Equal }
+    else if should_eval_sublist( arr_a , arr_b ) { eval_sublist(   arr_a,  arr_b ) }
+    else                                         { eval_superlist( arr_a , arr_b ) }
 }
